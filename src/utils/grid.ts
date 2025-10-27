e7ff1c72c2cce428e23aad09977a095511ba12e7
namnkahn1607 <nlnkandthebois@gmail.com>
2025-10-27 16:41:04 +0700

    26 Oct 25 add: matrix-like representation, tsconfig modify: more types on index.ts

---
+++ src/utils/grid.ts
@@ -0,0 +1,67 @@
+import type { GridNode, NodeType, Position } from "../types";
+
+const gridFunc = {
+    createNode: (
+        row: number, col: number, type: NodeType = 'empty'
+    ): GridNode => ({
+        position: { row, col },
+        type, g: Infinity, h: 0, f: Infinity,
+        parent: null
+    }),
+
+    initializeGrid: (rows: number, cols: number): GridNode[][] => {
+        const grid: GridNode[][] = [];
+
+        for (let i = 0; i < rows; ++i) {
+            grid[i] = [];
+
+            for (let j = 0; j < cols; ++j) {
+                grid[i][j] = gridFunc.createNode(i, j);
+            }
+        }
+
+        return grid;
+    },
+
+    getNeighbors: (
+        grid: GridNode[][], pos: Position, allowDiagonal: boolean
+    ): Position[] => {
+        const [row, col] = [pos.row, pos.col];
+        const neighbors: Array<Position> = [];
+
+        const directions = [
+            { row: -1, col: 0 }, { row: 0, col: -1 },
+            { row: 1, col: 0 }, { row: 0, col: 1},
+        ];
+
+        if (allowDiagonal) {
+            directions.push(
+                { row: -1, col: -1 }, { row: 1, col: -1 },
+                { row: -1, col: 1 }, { row: 1, col: 1 },
+            );
+        }
+
+        for (const dir of directions) {
+            const newRow = row + dir.row;
+            const newCol = col + dir.col;
+
+            if (newRow >= 0 && newRow < grid.length &&
+                newCol >= 0 && newCol < grid[0].length &&
+                grid[newRow][newCol].type != 'wall') {
+                neighbors.push({ row: newRow, col: newCol });
+            }
+        }
+
+        return neighbors;
+    },
+
+    positionEquals: (a: Position, b: Position): boolean => {
+        return a.row === b.row && a.col === b.col;
+    },
+
+    findPosition: (positions: Array<Position>, target: Position): number => {
+        return positions.findIndex(p => gridFunc.positionEquals(p, target));
+    }
+};
+
+export { gridFunc };
